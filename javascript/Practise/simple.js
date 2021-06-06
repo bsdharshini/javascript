742cc1cba2a6ce70a3002047d293aedda78a5f5b
Dharshini <bsdharshi@gmail.com>
2021-06-06 20:51:23 +0530

    practise

--- javascript/Practise/simple.js
+++ javascript/Practise/simple.js
@@ -1,24 +1,49 @@
 // if statement
-let cgpa = 8;
-if (cgpa>=9){
-    console.log("Outstanding");
-}
-else if(cgpa>=8 && cgpa<9){
-console.log("Great work");
-}
-else if(cgpa>=7 && cgpa<8){
-    console.log("Very good");
-}
-else if(cgpa>=6 && cgpa<7){
-    console.log("Good");
-}
-else if(cgpa>=5 && cgpa<6){
-    console.log("Need improvement");
-}
-else if(cgpa<5){
-console.log("Need more care");
+// let cgpa = 8;
+// if (cgpa>=9){
+//     console.log("Outstanding");
+// }
+// else if(cgpa>=8 && cgpa<9){
+// console.log("Great work");
+// }
+// else if(cgpa>=7 && cgpa<8){
+//     console.log("Very good");
+// }
+// else if(cgpa>=6 && cgpa<7){
+//     console.log("Good");
+// }
+// else if(cgpa>=5 && cgpa<6){
+//     console.log("Need improvement");
+// }
+// else if(cgpa<5){
+// console.log("Need more care");
+// }
+// else{
+//     console.log("Invalid cgpa");
+// }
+//decision making examples
+
+//nested if condition
+// if password is greater than 6, has number, has special character and has no space then login
+
+let password = parseInt(prompt("Enter password"));
+if(password.length>=6){
+    if(password.indexOf(' ')===-1)
+    {
+        console.log("You can login");
+    }
+    else{
+        console.log("Space not allowed")
+    }
 }
 else{
-    console.log("Invalid cgpa");
+    console.log("Must be min 6 length and no space");
 }
-//decision making examples
+
+//truth-y and false-y value
+// if(password){
+//     console.log("one");
+// }
+// else{
+//     console.log("two");
+// }
