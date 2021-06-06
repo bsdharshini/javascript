e03d254c8bdfb554f2c254d3bceccefe7d292305
Dharshini <bsdharshi@gmail.com>
2021-06-06 18:28:26 +0530

    Javascript example

---
+++ javascript/Practise/simple.js
@@ -0,0 +1,24 @@
+// if statement
+let cgpa = 8;
+if (cgpa>=9){
+    console.log("Outstanding");
+}
+else if(cgpa>=8 && cgpa<9){
+console.log("Great work");
+}
+else if(cgpa>=7 && cgpa<8){
+    console.log("Very good");
+}
+else if(cgpa>=6 && cgpa<7){
+    console.log("Good");
+}
+else if(cgpa>=5 && cgpa<6){
+    console.log("Need improvement");
+}
+else if(cgpa<5){
+console.log("Need more care");
+}
+else{
+    console.log("Invalid cgpa");
+}
+//decision making examples
